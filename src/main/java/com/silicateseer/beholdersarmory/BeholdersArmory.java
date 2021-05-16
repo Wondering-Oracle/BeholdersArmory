1035cbe54d20c0d2f8de0eca1ce7dd099f931c94
Wondering-Oracle <73407726+Wondering-Oracle@users.noreply.github.com>
2021-05-16 07:32:44 -0400

    Finish Setup

--- src/main/java/com/silicateseer/beholdersarmory/BeholdersArmory.java
+++ src/main/java/com/silicateseer/beholdersarmory/BeholdersArmory.java
@@ -1,14 +1,33 @@
-package net.fabricmc.example;
+package com.silicateseer.beholdersarmory;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
-public class ExampleMod implements ModInitializer {
+public class BeholdersArmory implements ModInitializer {
+	
+	public static final String MODID = "beholdersarmory";
+	
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+	}
+	
+	public static Block register(String name, Block block, ItemGroup tab) {
+		Registry.register(Registry.BLOCK, new Identifier(MODID, name), block);
+		BlockItem item = new BlockItem(block, new Item.Settings().group(tab));
+		register(name, item);
+		return block;
+	}
 
-		System.out.println("Hello Fabric world!");
+	public static Item register(String name, Item item) {
+		Registry.register(Registry.ITEM, new Identifier(MODID, name), item);
+		return item;
 	}
 }
